73c157622e2a3bee7b03557448ea3bfa5d2d1944
Ruslan Gaflanov <ruslan999_97@mail.ru>
2018-01-06 21:07:05 +0300

    Refactoring

--- src/main/java/controllers/LoginController.java
+++ src/main/java/controllers/LoginController.java
@@ -4,6 +4,7 @@ import annotation.Inject;
 import dao.UserDao;
 import entity.User;
 
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,25 +12,34 @@ import java.io.IOException;
 
 public class LoginController extends InjectionServlet {
 
-    public static final String MAIN_MENU_PAGE = "menu.jsp";
+    public static final String MAIN_MENU_PAGE = "login.jsp";
     public static final String USER_PAGE = "user.jsp";
     public static final String EMAIL = "email";
     public static final String PASSWORD = "password";
-
-//    @Inject("transactionManager")
-//    private TransactionManager transactionManager;
 
     @Inject("userDao")
     private UserDao userDao;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        User user = userDao.login(req.getParameter(EMAIL), req.getParameter(PASSWORD));
-        if(user == null){
-            req.getRequestDispatcher(MAIN_MENU_PAGE).forward(req, resp);
-            return;
-        }
-        req.setAttribute("user", user);
-        req.getRequestDispatcher(USER_PAGE).forward(req, resp);
+        final AsyncContext asyncСontext = req.startAsync();
+        asyncСontext.start(()->{
+            try{
+                User user = userDao.login(req.getParameter(EMAIL), req.getParameter(PASSWORD));
+
+                if(user == null) req.getRequestDispatcher(MAIN_MENU_PAGE).forward(req, resp);
+
+                req.setAttribute("user", user);
+                req.getRequestDispatcher(USER_PAGE).forward(req, resp);
+
+            } catch (ServletException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally {
+                asyncСontext.complete();
+            }
+        });
+
     }
 }
